057a07d709497e77c026daf2042a2d7b0f66cb53
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2021-08-20 22:42:40 -0300

    tentativa 1

--- Assets/JS/register.js
+++ Assets/JS/register.js
@@ -3,6 +3,10 @@ function load() {
         n.style.borderColor = 'rgb(238, 98, 98)'
         msg.style.display = 'block'
     }
+    function normal(n, msg){
+        n.style.borderColor = 'lightgray'
+        msg.style.display = 'none'
+    }
     var error1 = document.getElementById('error-password1')
     var error2 = document.getElementById('error-password2')
     var error3 = document.getElementById('error-password3')
@@ -10,31 +14,26 @@ function load() {
     
     var tel = document.getElementById('tel')
     tel.addEventListener('click', function() {
-        tel.style.borderColor = 'lightgray' 
-        error1.style.display = 'none'
+        normal(tel, error1)
     })
 
     var fullName = document.getElementById('fullName')
     fullName.addEventListener('click', function() {
-        fullName.style.borderColor = 'lightgray' 
-        error2.style.display = 'none'
+        normal(fullName, error2)
     })
 
     var name = document.getElementById('name')
     name.addEventListener('click', function() {
-        name.style.borderColor = 'lightgray' 
-        error3.style.display = 'none'
+        normal(name, error3)
     })
 
     var password = document.getElementById('password')
     password.addEventListener('click', function() {
-        password.style.borderColor = 'lightgray' 
-        error4.style.display = 'none'
+        normal(password, error4)
     })
 
     var btn = document.getElementById('btn')
     btn.addEventListener('click', function(){
-        
         if(password.value == "" && tel.value == "" && fullName.value == "" && name.value == ""){
             active(tel, error1)
             active(fullName, error2)
@@ -78,52 +77,35 @@ function load() {
             active(password, error4)
 
         }else if(fullName.value == ""){
-            fulName.style.borderColor = 'rgb(238, 98, 98)'
-            error2.style.display = 'block'
+            active(fullName, error2)
 
         }else if(fullName.value == "" && name.value == ""){
-            fullName.style.borderColor = 'rgb(238, 98, 98)'
-            name.style.borderColor = 'rgb(238, 98, 98)'
-
-            error2.style.display = 'block'
-            error3.style.display = 'block'
+            active(fullName, error2)
+            active(name, error3)
 
         }else if(fullName.value == "" && password.value == ""){
-            fullName.style.borderColor = 'rgb(238, 98, 98)'
-            password.style.borderColor = 'rgb(238, 98, 98)'
-
-            error2.style.display = 'block'
-            error4.style.display = 'block'
+            active(fullName, error2)
+            active(password, error4)
 
         }else if(name.value == ""){
-            name.style.borderColor = 'rgb(238, 98, 98)'
-            error3.style.display = 'block'
+            active(name, error3)
 
         }else if(name.value == "" && password.value == ""){
-            name.style.borderColor = 'rgb(238, 98, 98)'
-            password.style.borderColor = 'rgb(238, 98, 98)'
-
-            error3.style.display = 'block'
-            error4.style.display = 'block'
+            active(name, error3)
+            active(password, error4)
 
         }else if(password.value == ""){
-            password.style.borderColor = 'rgb(238, 98, 98)'
-            error4.style.display = 'block'
+            active(password, error4)
 
         }else if( password.value.length < 8 && tel.value.length < 11) {
-            tel.style.borderColor = 'rgb(238, 98, 98)' 
-            error1.style.display = 'block'
-
-            password.style.borderColor = 'rgb(238, 98, 98)' 
-            error4.style.display = 'block'
+            active(tel, error1)
+            active(password, error4)
         
         }else if(tel.value.length < 11){
-            tel.style.borderColor = 'rgb(238, 98, 98)' 
-            error1.style.display = 'block'
+            active(tel, error1)
         
         }else if(password.value.length < 8){
-            password.style.borderColor = 'rgb(238, 98, 98)' 
-            error4.style.display = 'block'
+            active(password, error4)
 
         }else{
             window.location.href = "../../index.html"
