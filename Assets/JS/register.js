6e7c35aa5445faac50d2e091d81b22c7d36d42e4
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2021-08-20 23:21:33 -0300

    Arrumei o js
    Adicionei a função active deixando o código mais enxuto e mais funcional, a função passa parametros

--- Assets/JS/register.js
+++ Assets/JS/register.js
@@ -1,11 +1,7 @@
 function load() {
-    function active(n, msg){
-        n.style.borderColor = 'rgb(238, 98, 98)'
-        msg.style.display = 'block'
-    }
-    function normal(n, msg){
-        n.style.borderColor = 'lightgray'
-        msg.style.display = 'none'
+    function active(n, msg, color,b){
+        n.style.borderColor = color
+        msg.style.display = b
     }
     var error1 = document.getElementById('error-password1')
     var error2 = document.getElementById('error-password2')
@@ -14,98 +10,97 @@ function load() {
     
     var tel = document.getElementById('tel')
     tel.addEventListener('click', function() {
-        normal(tel, error1)
+        active(tel, error1, 'lightgray', 'none')
     })
 
     var fullName = document.getElementById('fullName')
     fullName.addEventListener('click', function() {
-        normal(fullName, error2)
+        active(fullName, error2, 'lightgray', 'none')
     })
 
     var name = document.getElementById('name')
     name.addEventListener('click', function() {
-        normal(name, error3)
+        active(name, error3, 'lightgray', 'none')
     })
 
     var password = document.getElementById('password')
     password.addEventListener('click', function() {
-        normal(password, error4)
+        active(password, error4, 'lightgray', 'none')
     })
 
     var btn = document.getElementById('btn')
     btn.addEventListener('click', function(){
         if(password.value == "" && tel.value == "" && fullName.value == "" && name.value == ""){
-            active(tel, error1)
-            active(fullName, error2)
-            active(name, error3)
-            active(password, error4)
+            active(tel, error1, 'rgb(238, 98, 98)', 'block')
+            active(fullName, error2, 'rgb(238, 98, 98)', 'block')
+            active(name, error3, 'rgb(238, 98, 98)', 'block')
+            active(password, error4, 'rgb(238, 98, 98)', 'block')
 
         }else if(tel.value == "" && fullName.value == "" && name.value == ""){
-            active(tel, error1)
-            active(fullName, error2)
-            active(name, error3)
+            active(tel, error1, 'rgb(238, 98, 98)', 'block')
+            active(fullName, error2, 'rgb(238, 98, 98)', 'block')
+            active(name, error3, 'rgb(238, 98, 98)', 'block')
 
         }else if(fullName.value == "" && name.value == "" && password.value == ""){
-            active(fullName, error2)
-            active(name, error3)
-            active(password, error4)
+            active(fullName, error2, 'rgb(238, 98, 98)', 'block')
+            active(name, error3, 'rgb(238, 98, 98)', 'block')
+            active(password, error4, 'rgb(238, 98, 98)', 'block')
 
         }else if(tel.value == "" && name.value == "" && password.value == ""){
-            active(tel, error1)
-            active(name, error3)
-            active(password, error4)
+            active(tel, error1, 'rgb(238, 98, 98)', 'block')
+            active(name, error3, 'rgb(238, 98, 98)', 'block')
+            active(password, error4, 'rgb(238, 98, 98)', 'block')
             
         }else if(tel.value == "" && fullName.value == "" && password.value == ""){
-            active(tel, error1)
-            active(fullName, error2)
-            active(password, error4)
-
-        }
-        else if(tel.value == ""){
-            active1(tel, error1)
+            active(tel, error1, 'rgb(238, 98, 98)', 'block')
+            active(fullName, error2, 'rgb(238, 98, 98)', 'block')
+            active(password, error4, 'rgb(238, 98, 98)', 'block')
 
         }else if(tel.value == "" && fullName.value == ""){
-            active(tel, error1)
-            active(fullName, error2)
+            active(tel, error1, 'rgb(238, 98, 98)', 'block')
+            active(fullName, error2, 'rgb(238, 98, 98)', 'block')
 
         }else if(tel.value == "" && name.value == ""){
-            active(tel, error1)
-            active(name, error3)
+            active(tel, error1, 'rgb(238, 98, 98)', 'block')
+            active(name, error3, 'rgb(238, 98, 98)', 'block')
 
         }else if(tel.value == "" && password.value == ""){
-            active(tel, error1)
-            active(password, error4)
-
-        }else if(fullName.value == ""){
-            active(fullName, error2)
+            active(tel, error1, 'rgb(238, 98, 98)', 'block')
+            active(password, error4, 'rgb(238, 98, 98)', 'block')
 
         }else if(fullName.value == "" && name.value == ""){
-            active(fullName, error2)
-            active(name, error3)
+            active(fullName, error2, 'rgb(238, 98, 98)', 'block')
+            active(name, error3, 'rgb(238, 98, 98)', 'block')
 
         }else if(fullName.value == "" && password.value == ""){
-            active(fullName, error2)
-            active(password, error4)
-
-        }else if(name.value == ""){
-            active(name, error3)
+            active(fullName, error2, 'rgb(238, 98, 98)', 'block')
+            active(password, error4, 'rgb(238, 98, 98)', 'block')
 
         }else if(name.value == "" && password.value == ""){
-            active(name, error3)
-            active(password, error4)
+            active(name, error3, 'rgb(238, 98, 98)', 'block')
+            active(password, error4, 'rgb(238, 98, 98)', 'block')
+
+        }else if(tel.value == ""){
+            active(tel, error1, 'rgb(238, 98, 98)', 'block')
+
+        }else if(fullName.value == ""){
+            active(fullName, error2, 'rgb(238, 98, 98)', 'block')
+
+        }else if(name.value == ""){
+            active(name, error3, 'rgb(238, 98, 98)', 'block')
 
         }else if(password.value == ""){
-            active(password, error4)
+            active(password, error4, 'rgb(238, 98, 98)', 'block')
 
         }else if( password.value.length < 8 && tel.value.length < 11) {
-            active(tel, error1)
-            active(password, error4)
+            active(tel, error1, 'rgb(238, 98, 98)', 'block')
+            active(password, error4, 'rgb(238, 98, 98)', 'block')
         
         }else if(tel.value.length < 11){
-            active(tel, error1)
+            active(tel, error1, 'rgb(238, 98, 98)', 'block')
         
         }else if(password.value.length < 8){
-            active(password, error4)
+            active(password, error4, 'rgb(238, 98, 98)', 'block')
 
         }else{
             window.location.href = "../../index.html"
--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,37 +1,32 @@
 function load() {
+    function active(n, msg, color,b){
+        n.style.borderColor = color
+        msg.style.display = b
+    }
     var error1 = document.getElementById('error-password1')
     var error2 = document.getElementById('error-password2')
 
     var email = document.getElementById('email')
     email.addEventListener('click', function() {
-        email.style.borderColor = 'lightgray' 
-        error1.style.display = 'none'
+        active(email, error1, 'lightgray', 'none' )
     })
     var password = document.getElementById('password')
     password.addEventListener('click', function() {
-        password.style.borderColor = 'lightgray' 
-        error2.style.display = 'none'
+        active(password, error2,  'lightgray', 'none')
     })
 
     var btn = document.getElementById('button')
     btn.addEventListener('click', function(){
         if (email.value == "" && password.value == ""){
-            email.style.borderColor = 'rgb(238, 98, 98)' 
-            password.style.borderColor = 'rgb(238, 98, 98)' 
-            error1.style.display = 'block'
-            error2.style.display = 'block'
+            active(email, error1, 'rgb(238, 98, 98)' , 'block')
+            active(password, error2, 'rgb(238, 98, 98)' , 'block')
 
         }else if(email.value == ""){
-            email.style.borderColor = 'rgb(238, 98, 98)' 
-            error1.style.display = 'block'
-
+            active(email, error1, 'rgb(238, 98, 98)' , 'block')
         }else if(password.value == ""){
-            password.style.borderColor = 'rgb(238, 98, 98)' 
-            error2.style.display = 'block'
-
+            active(password, error2, 'rgb(238, 98, 98)' , 'block')
         }else if(password.value.length < 8){
-            password.style.borderColor = 'rgb(238, 98, 98)' 
-            error2.style.display = 'block'
+            active(password, error2, 'rgb(238, 98, 98)' , 'block')
         }else{
             window.location.href = "Assets/HTML/logged.html"
         }
